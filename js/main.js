61ad122b8c6f67bd129a31fb0e2cdaf88fee4fb3
Tony Haddon <tony.haddon@gmail.com>
2014-11-18 22:12:22 +0000

    19. An array of objects

--- js/main.js
+++ js/main.js
@@ -3,20 +3,28 @@ $(document).ready(function() {
 	var b = 0;
 	var week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
-	var vehicle = {
-	"name": "car",
-	"wheels" : 4,
-	"colour" : "red",
-	"wings" : 0
-     };
+	var vehicles = [
+		{
+			"name": "car",
+			"wheels" : 4,
+			"colour" : "red",
+			"wings" : 0
+	    },
+		{
+			"name": "lorry",
+			"wheels" : 18,
+			"colour" : "blue",
+			"wings" : 0
+		}
+	];
 
 	$(".firstbutton").click(function() {
 		
 		$("#vehicle-number").text(b+1);
-		$("#vehicle-name").text(vehicle.name);
-		$("#vehicle-wheels").text(vehicle.wheels);
-		$("#vehicle-colour").text(vehicle.colour);
-		$("#vehicle-wings").text(vehicle.wings);
+		$("#vehicle-name").text(vehicles[b].name);
+		$("#vehicle-wheels").text(vehicles[b].wheels);
+		$("#vehicle-colour").text(vehicles[b].colour);
+		$("#vehicle-wings").text(vehicles[b].wings);
 
 		$("#week-day").text(week[b]);
 
