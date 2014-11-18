16e88c9704d09c50eabbb3934894d8383ca62883
Tony Haddon <tony.haddon@gmail.com>
2014-11-18 22:08:10 +0000

    What is an object?

--- js/main.js
+++ js/main.js
@@ -3,9 +3,23 @@ $(document).ready(function() {
 	var b = 0;
 	var week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
+	var vehicle = {
+	"name": "car",
+	"wheels" : 4,
+	"colour" : "red",
+	"wings" : 0
+     };
+
 	$(".firstbutton").click(function() {
-		if (b>6) b=0;
-		$(".content").append("<div>" + week[b] + "</div>");
+		
+		$("#vehicle-number").text(b+1);
+		$("#vehicle-name").text(vehicle.name);
+		$("#vehicle-wheels").text(vehicle.wheels);
+		$("#vehicle-colour").text(vehicle.colour);
+		$("#vehicle-wings").text(vehicle.wings);
+
+		$("#week-day").text(week[b]);
+
 		b = b + 1;
 
 	});
