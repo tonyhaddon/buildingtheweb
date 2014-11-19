505ddb32f055e5225b2c6ba0fdd1e1ce4ff51e96
Tony Haddon <tony.haddon@gmail.com>
2014-11-19 23:27:25 +0000

    What is an object update

--- js/main.js
+++ js/main.js
@@ -3,9 +3,21 @@ $(document).ready(function() {
 	var b = 0;
 	var week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
+	var vehicle = {
+	"name": "car",
+	"wheels" : 4,
+	"colour" : "red",
+	"wings" : 0
+     };
+
 	$(".firstbutton").click(function() {
 		
-		$(".content").append("<div>" + b + "</div>");
+		$("#vehicle-number").text(b+1);
+		$("#vehicle-name").text(vehicle.name);
+		// Output some more properties here...
+
+		$("#week-day").text(week[b]);
+
 		b = b + 1;
 
 	});
