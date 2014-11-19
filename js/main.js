b06cf664685ab24b0677fcd8ba4131e4283606ad
Tony Haddon <tony.haddon@gmail.com>
2014-11-19 18:01:40 +0000

    Update to 16

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
 
 	var b = 0;
-	var week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
 	$(".firstbutton").click(function() {
 		if (b>6) b=0;
-		$(".content").append("<div>" + week[b] + "</div>");
+		$(".content").append("<div>" + b + "</div>");
 		b = b + 1;
 
 	});
