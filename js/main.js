cbe126223e8c8821908169af5a391ab85922f615
Tony Haddon <tony.haddon@gmail.com>
2014-11-20 20:53:01 +0000

    4 vehicles, and a full template. But needs some better styling!

--- js/main.js
+++ js/main.js
@@ -3,20 +3,46 @@ $(document).ready(function() {
 	var b = 0;
 	var week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
-	var vehicle = {
-	"name": "car",
-	"wheels" : 4,
-	"colour" : "red",
-	"wings" : 0
-     };
+	var vehicles = [
+		{
+			"name": "car",
+			"wheels" : 4,
+			"colour" : "red",
+			"wings" : 0
+	    },
+		{
+			"name": "lorry",
+			"wheels" : 18,
+			"colour" : "blue",
+			"wings" : 0
+		},
+		{
+			"name": "helicopter",
+			"wheels" : 0,
+			"colour" : "orange",
+			"wings" : 4
+		},
+		{
+			"name": "aeroplane",
+			"wheels" : 12,
+			"colour" : "yellow",
+			"wings" : 2
+		},
+
+	];
 
 	$(".firstbutton").click(function() {
 		
-		$("#vehicle-number").text(b+1);
-		$("#vehicle-name").text(vehicle.name);
-		// Output some more properties here...
+		if (b >= vehicles.length)
+		{
+			b = 0;
+		}
 
-		$("#week-day").text(week[b]);
+		$("#vehicle-number").text(b+1);
+		$("#vehicle-name").text(vehicles[b].name);
+		$("#vehicle-wheels").text(vehicles[b].wheels);
+		$("#vehicle-colour").text(vehicles[b].colour);
+		$("#vehicle-wings").text(vehicles[b].wings);
 
 		b = b + 1;
 
