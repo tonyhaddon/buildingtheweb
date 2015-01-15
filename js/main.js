2ae741a23153b6da77a79217666e7e420fd285e8
Tony Haddon <tony.haddon@gmail.com>
2015-01-15 23:33:01 +0000

    DL plus styling

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
+
 $(document).ready(function() {
 
 	var b = 0;
-	var week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-
-	var vehicles = [
+	
+    var vehicles = [
 		{
 			"name": "car",
 			"wheels" : 4,
 			"colour" : "red",
 			"wings" : 0
-	    },
+		},
 		{
 			"name": "lorry",
 			"wheels" : 18,
@@ -17,35 +17,31 @@ $(document).ready(function() {
 			"wings" : 0
 		},
 		{
-			"name": "helicopter",
-			"wheels" : 0,
-			"colour" : "orange",
-			"wings" : 4
-		},
-		{
-			"name": "aeroplane",
-			"wheels" : 12,
-			"colour" : "yellow",
+			"name": "airplane",
+			"wheels" : 6,
+			"colour" : "white",
 			"wings" : 2
-		},
-
-	];
+		},	
+		{
+			"name": "speedboat",
+			"wheels" : 0,
+			"colour" : "yellow",
+			"wings" : 0
+		}	
+	]
 
 	$(".firstbutton").click(function() {
 		
-		if (b >= vehicles.length)
-		{
-			b = 0;
-		}
+		if (b >= vehicles.length) b=0;
+
 
 		$("#vehicle-number").text(b+1);
 		$("#vehicle-name").text(vehicles[b].name);
-		$("#vehicle-wheels").text(vehicles[b].wheels);
+		$("#vehicle-number-wheels").text(vehicles[b].wheels);
 		$("#vehicle-colour").text(vehicles[b].colour);
-		$("#vehicle-wings").text(vehicles[b].wings);
+		$("#vehicle-number-wings").text(vehicles[b].wings);
 
 		b = b + 1;
 
 	});
-
 });
